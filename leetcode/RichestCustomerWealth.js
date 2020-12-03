3f2646f6de2f96048bf2dbea854048a896878542
Jet <jethro.talavera1@gmail.com>
2020-12-03 13:51:23 -0500

    new leetcode solution

---
+++ leetcode/RichestCustomerWealth.js
@@ -0,0 +1,22 @@
+// leetcode 1672 my solution
+
+// A customer's wealth is the amount of money they have in all their bank accounts.
+// The richest customer is the customer that has the maximum wealth.
+
+// EXAMPLE 1
+// Input: accounts = [[1,2,3],[3,2,1]]
+// Output: 6
+// Explanation:
+// 1st customer has wealth = 1 + 2 + 3 = 6
+// 2nd customer has wealth = 3 + 2 + 1 = 6
+// Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+var maximumWealth = function(accounts) {
+
+    let x = accounts.map((account) => {
+        return account.reduce((a, b) => a + b, 0)
+    })
+
+    return Math.max(...x)
+
+};
